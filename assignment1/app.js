5a7e0fbf26d48a40f3ee2ed67ae9fbecc13a5bcd
Tarun Kumar Singh <mr.tksingh@gmail.com>
2017-01-09 18:39:49 +0530

    module 1 completed

--- assignment1/app.js
+++ assignment1/app.js
@@ -1,11 +1,31 @@
 (function(){
 "use strict";
-angular.module("myFirstApp",[])
+angular.module("LunchCheck",[])
+.controller('LunchCheckController', LunchCfunc);
 
-
-.controller('myFirstController', function($scope){
-	$scope.name ="tango";
-	
-});
+LunchCfunc.$inject =["$scope"];
+function LunchCfunc($scope){
+	$scope.input_val ="";
+	$scope.message = '';
+	$scope.class ='';
+	$scope.showMsg = function(){
+		var array = $scope.input_val.split(',');
+		array = array.filter(function(str) {
+   	 		return /\S/.test(str);
+		});
+		if(array.length < 1){
+			$scope.message ="Please enter data first";
+			$scope.class = "red";
+		}
+		else if(array.length < 3){
+			$scope.message =array.length+" Item. Enjoy!";
+			$scope.class = "green";
+		}
+		else{
+			$scope.message =array.length+" Item. Too much!";
+			$scope.class = "green";
+		}		
+	};
+};
 
 })();
